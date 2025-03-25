cf5bd07362b51da397e28a7509e94eeda276e1fe
Ian Edington <ianedington@gpo.ca>
2025-03-25 13:31:45 -0400

    feat: update EO reports based on tech specs

--- google-workspace/tax_receipts/tax_receipts_split_main_into_eo_reports.js
+++ google-workspace/tax_receipts/tax_receipts_split_main_into_eo_reports.js
@@ -1,88 +1,56 @@
+/**
+ *
+ *
+ * If you need to
+ */
+
+// TODO: filter out Receipt_Status = C & L only I (Issued) should be kept
+
+/**
+ * Add a menu to the spreadsheet so the Admin team can run this task
+ */
 function onOpen() {
-  var ui = SpreadsheetApp.getUi();
+  const ui = SpreadsheetApp.getUi();
   ui.createMenu('Custom Scripts')
-    .addItem('Export CSVs', 'exportFilteredCSVs')
+    .addItem('Generate Reports as CSVs', 'exportFilteredCSVs')
     .addToUi();
 }
 
 function exportFilteredCSVs() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getActiveSheet();
-  var folder = getOrCreateExportFolder(ss);
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
 
-  var data = sheet.getDataRange().getValues();
-  if (data.length < 2) {
-    Logger.log('No data found.');
-    return;
-  }
+  const generatedFilesFolder = getOrCreateExportFolder(ss);
 
-  var headers = data[0]; // First row as headers
-  var records = data.slice(1); // Data excluding headers
+  const { headers, reports } = segmentReports(ss);
 
-  var groupedData = {};
-  var s2p2Report = [];
+  const reportingPeriodNamesMap = getPeriodNames(ss);
 
-  records.forEach((row) => {
-    var politicalEntity = row[6]; // Column G
-    var reportPeriod = row[11]; // Column L
-
-    if (!politicalEntity || !reportPeriod) {
-      throw Error(
-        `row is missing a political entity: ${politicalEntity} or a report period: ${reportPeriod}. ${convertToCSV([row])}`,
-      );
-    }
-
-    if (!groupedData[reportPeriod]) {
-      groupedData[reportPeriod] = {};
-    }
-
-    if (!groupedData[reportPeriod][politicalEntity]) {
-      groupedData[reportPeriod][politicalEntity] = [];
-    }
-
-    groupedData[reportPeriod][politicalEntity].push(row);
-
-    var amount = Number(row[7]);
-    if (amount >= 200.01 && politicalEntity.trim() === 'GPO') {
-      s2p2Report.push(row);
-    }
-  });
-
-  for (var reportPeriod in groupedData) {
-    for (var politicalEntity in groupedData[reportPeriod]) {
-      var fileName = `Period ${reportPeriod} - ${politicalEntity}.csv`;
-
-      //var csvContent = convertToCSV([headers].concat(groupedData[reportPeriod][politicalEntity]));
-      //saveCSV(fileName, csvContent, folder);
-    }
-  }
-
-  var s2p2CsvContent = convertToCSV([headers].concat(s2p2Report));
-  saveCSV('s2p2 report.csv', s2p2CsvContent, folder);
+  saveReports(headers, reports, reportingPeriodNamesMap, generatedFilesFolder);
 }
 
 function getOrCreateExportFolder(ss) {
-  var masterFile = DriveApp.getFileById(ss.getId());
-  var parentFolder = masterFile.getParents().next();
+  const folderName = 'EO Contribution Reports (Generated)';
+  const mainFile = DriveApp.getFileById(ss.getId());
+  const parentFolder = mainFile.getParents().next();
 
   // Check if the folder exists
-  var folders = parentFolder.getFoldersByName('csv_exports');
+  const folders = parentFolder.getFoldersByName(folderName);
   while (folders.hasNext()) {
-    var folder = folders.next();
+    const folder = folders.next();
     deleteFolderAndContents(folder); // Delete the folder and all its contents
   }
 
   // Create a new folder
-  return parentFolder.createFolder('csv_exports');
+  return parentFolder.createFolder(folderName);
 }
 
 function deleteFolderAndContents(folder) {
-  var files = folder.getFiles();
+  const files = folder.getFiles();
   while (files.hasNext()) {
     files.next().setTrashed(true); // Move files to trash
   }
 
-  var subfolders = folder.getFolders();
+  const subfolders = folder.getFolders();
   while (subfolders.hasNext()) {
     deleteFolderAndContents(subfolders.next()); // Recursively delete subfolders
   }
@@ -90,17 +58,174 @@ function deleteFolderAndContents(folder) {
   folder.setTrashed(true); // Finally, delete the folder itself
 }
 
+function segmentReports(ss) {
+  const sheetName = 'FINAL';
+  const data = ss.getSheetByName(sheetName).getDataRange().getValues();
+  if (data.length < 2) {
+    throwAndDisplayError(`No data found in sheet ${sheetName}`);
+  }
+
+  const reports = reportsObject();
+
+  const headers = data[0]; // First row as headers
+
+  const records = data.slice(1); // Data excluding headers
+  records.forEach((row) => {
+    let politicalEntity = row[6]; // Column G
+    if (politicalEntity.startsWith('GPO ')) {
+      politicalEntity = politicalEntity.substring(4);
+    }
+    const reportingPeriod = row[11]; // Column L
+
+    if (!politicalEntity || !reportingPeriod) {
+      throwAndDisplayError(
+        `row is missing a political entity: ${politicalEntity} ` +
+          `or a reporting period: ${reportingPeriod}. ${convertToCSV([row])}`,
+      );
+    }
+
+    reports[reportingPeriod].all.push(row);
+
+    const amount = Number(row[7]);
+    if (amount >= 200.01) {
+      reports[reportingPeriod].s2p2.push(row);
+    }
+
+    reports[reportingPeriod].entityReports[politicalEntity].push(row);
+  });
+
+  return { headers, reports };
+}
+
+function getPeriodNames(ss) {
+  const data = ss
+    .getSheetByName('Reporting_Periods')
+    .getDataRange()
+    .getValues();
+  const records = data.slice(1); // Data excluding headers
+
+  return records.reduce((map, row) => {
+    const periodNumber = row[0];
+    const periodName = row[1];
+
+    map[periodNumber] = periodName;
+    return map;
+  }, {});
+}
+
+function saveReports(
+  headers,
+  reports,
+  reportingPeriodNamesMap,
+  generatedFilesFolder,
+) {
+  for (const reportingPeriod in reports) {
+    const report = reports[reportingPeriod];
+    const reportingPeriodName = reportingPeriodNamesMap[reportingPeriod];
+    const reportingPeriodFolder = findOrCreateFolder(
+      generatedFilesFolder,
+      `${reportingPeriod} - ${reportingPeriodName}`,
+    );
+
+    // Save the file with the following naming convention: Party + ED + Event + ALL (ie. ABC 123 2023 Annual ALL)
+    const allReportFileName = `GPO ${reportingPeriodName} ALL.csv`;
+
+    const allReportCsvContent = convertToCSV([headers].concat(report.all));
+    saveCSV(allReportFileName, allReportCsvContent, reportingPeriodFolder);
+
+    // Save the file with the following naming convention: Party + ED + Event + S2P2 (ie. ABC 123 2023 Annual S2P2)
+    const s2p2ReportFileName = `GPO ${reportingPeriodName} S2P2.csv`;
+
+    const s2p2CsvContent = convertToCSV([headers].concat(report.s2p2));
+    saveCSV(s2p2ReportFileName, s2p2CsvContent, reportingPeriodFolder);
+
+    const entityReportsFolder = findOrCreateFolder(
+      reportingPeriodFolder,
+      'entity reports',
+    );
+
+    for (const politicalEntity in report.entityReports) {
+      // Save the file with the following naming convention: Party + ED + Event + ALL (ie. ABC 123 2023 Annual ALL)
+      const fileName = `GPO ${reportingPeriodName} ${politicalEntity}.csv`;
+
+      const csvContent = convertToCSV(
+        [headers].concat(report.entityReports[politicalEntity]),
+      );
+      saveCSV(fileName, csvContent, entityReportsFolder);
+    }
+  }
+}
+
+function findOrCreateFolder(parentFolder, folderName) {
+  const folders = parentFolder.getFoldersByName(folderName);
+  while (folders.hasNext()) {
+    return folders.next();
+  }
+  return parentFolder.createFolder(folderName);
+}
+
 function convertToCSV(dataArray) {
   return dataArray
-    .map((row) => row.map((value) => `"${value}"`).join(','))
+    .map((row) =>
+      row
+        .map((value) => {
+          if (value instanceof Date) {
+            return formatDateToMMDDYYYY(value);
+          }
+
+          return `"${value}"`;
+        })
+        .join(','),
+    )
     .join('\n');
 }
 
+function formatDateToMMDDYYYY(date) {
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  const day = String(date.getDate()).padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${month}${day}${year}`;
+}
+
 function saveCSV(fileName, content, folder) {
-  var files = folder.getFilesByName(fileName);
+  const files = folder.getFilesByName(fileName);
   while (files.hasNext()) {
     files.next().setTrashed(true);
   }
 
   folder.createFile(fileName, content, MimeType.CSV);
+}
+
+function throwAndDisplayError(message) {
+  SpreadsheetApp.getUi().alert(message);
+  throw new Error(message);
+}
+
+function mapWithDefaultArray() {
+  return new Proxy(
+    {},
+    {
+      get: (target, name) =>
+        name in target ? target[name] : (target[name] = []),
+    },
+  );
+}
+
+class ReportingPeriod {
+  constructor() {
+    this.all = [];
+    this.s2p2 = [];
+    this.entityReports = mapWithDefaultArray();
+  }
+}
+
+function reportsObject() {
+  return new Proxy(
+    {},
+    {
+      get: (target, name) =>
+        name in target ? target[name] : (target[name] = new ReportingPeriod()),
+    },
+  );
 }
