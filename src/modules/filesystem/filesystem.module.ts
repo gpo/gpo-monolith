3af7245d340a3d66d05a53e55b71d242f2bea685
Ian Edington <ian.edington@gmail.com>
2026-06-03 05:56:16 -0400

    Remove unused NestJS application and related infrastructure (#89)

--- src/modules/filesystem/filesystem.module.ts
+++
@@ -1,9 +0,0 @@
-import {Module} from '@nestjs/common';
-import {FilesystemService} from "./filesystem.service";
-
-@Module({
-    providers: [FilesystemService],
-    exports: [FilesystemService],
-})
-export class FilesystemModule {
-}
